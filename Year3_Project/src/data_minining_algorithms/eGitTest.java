7cbb334ce9e2d6bf4a4159ce867beeb089bcce3a
Elliot <Elliotsul@aol.com>
2013-01-20 16:07:02 +0000

    Testing class created for EGit
    Signed-off-by: Elliot <Elliotsul@aol.com>

---
+++ Year3_Project/src/data_minining_algorithms/eGitTest.java
@@ -0,0 +1,13 @@
+package data_minining_algorithms;
+
+public class eGitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
