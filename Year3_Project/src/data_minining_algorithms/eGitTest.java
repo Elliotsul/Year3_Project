265f158e9ce2fe701309572ef91699843811e528
Elliot <Elliotsul@aol.com>
2013-01-23 19:17:53 +0000

    New github tests

--- Year3_Project/src/data_minining_algorithms/eGitTest.java
+++ Year3_Project/src/data_minining_algorithms/eGitTest.java
@@ -7,6 +7,8 @@ public class eGitTest {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Testing EGit");
+		System.out.println("Testing EGit 2");
+		System.out.println("Testing again since install of Git for Mac");
 
 	}
 
