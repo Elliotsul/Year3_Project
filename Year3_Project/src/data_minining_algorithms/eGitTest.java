713797641e8529fa421a8be7ae73388893f3643b
Elliot <Elliotsul@aol.com>
2013-02-07 14:49:44 +0000

    Full commit

---
+++ Year3_Project/src/data_minining_algorithms/eGitTest.java
@@ -0,0 +1,15 @@
+package data_minining_algorithms;
+
+public class eGitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Testing EGit");
+		System.out.println("Testing EGit 2");
+		System.out.println("Testing again since install of Git for Mac");
+
+	}
+
+}
