f1341f9088a4f711c8740ec475a371c28b847aa5
Elliot <Elliotsul@aol.com>
2013-02-07 18:55:11 +0000

    request type code added
    - arrays created
    - method stubs created

--- Year3_Project/src/data_minining_algorithms/dataSetup.java
+++ Year3_Project/src/data_minining_algorithms/dataSetup.java
@@ -8,31 +8,52 @@ import au.com.bytecode.opencsv.*;
 public class dataSetup {
 	
 	static String datasetfile = "Year3_Project/Data/Request_analysis.csv";
+	static CSVReader reader;
+	static String [][] data = new String[845][7];
+	static String [][] london = new String[845][4];
+	static String [][] county = new String[845][4];
+	static String [][] crown = new String[845][4];
+	static String [] nextRecord = new String[7];
 
 		public static void main(String[] args) throws IOException {
-
-		CSVReader reader = new CSVReader(new FileReader(datasetfile));
-		String [][] data = new String[845][7];
-		String [] nextRecord = new String[7];
-       
-		int m =0;
-		while ((nextRecord = reader.readNext()) != null) {
-			int k;
-			for(k=0;k<7;k++){
-				data[m][k]=nextRecord[k];
-		}
-			m++; 
+			reader = new CSVReader(new FileReader(datasetfile));
+			matrixSetup();
+			printDataset();
+			
 		}
 
+      
 
-		for(int j = 0 ; j < m; j++){
+		
+	static void printDataset(){	
+		for(int j = 0 ; j < 844; j++){
 			for (int i =0; i < 7; i++){
 				System.out.print(data[j][i]);
 				System.out.print(", ");
 			}
 			System.out.println();
 		}
-
 	}
+	
+	public static void matrixSetup() throws IOException{
+		int m =0;
+		while ((nextRecord = reader.readNext()) != null) {
+		int k;
+		for(k=0;k<7;k++){
+			data[m][k]=nextRecord[k];
+		}
+			m++; 
+		}
+	}
+	
+	public static void londonSetup() {
+		
+		for (int i = 0; i < 845; i++){
+				data[i][3] = london[0][0];
+				
+			}
+		}
+	}	
 
-}
+
+
