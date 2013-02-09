d0ad75a084a8a03beaf02caf9793fa7db44159d9
Elliot <Elliotsul@aol.com>
2013-02-09 14:24:05 +0000

    Windowing code written

--- Year3_Project/src/data_minining_algorithms/dataSetup.java
+++ Year3_Project/src/data_minining_algorithms/dataSetup.java
@@ -10,25 +10,31 @@ public class dataSetup {
 	static String datasetfile = "Year3_Project/Data/Request_analysis.csv";
 	static CSVReader reader;
 	static String [][] data = new String[845][7];
-	static String [][] london = new String[845][4];
-	static String [][] county = new String[845][4];
-	static String [][] crown = new String[845][4];
+	static String [][] london = new String[845][5];
+	static String [] temp = new String[845];
+	static String [][] county = new String[845][5];
+	static String [][] crown = new String[845][5];
 	static String [] nextRecord = new String[7];
+	static boolean first = true;
+	static int x = 0;
+	static int y = 0;
+	static int width = 4;
+	static int height = 845;
 
 		public static void main(String[] args) throws IOException {
 			reader = new CSVReader(new FileReader(datasetfile));
 			matrixSetup();
+			londonSetup();
 			printDataset();
 			
 		}
 
-      
 
-		
 	static void printDataset(){	
-		for(int j = 0 ; j < 844; j++){
-			for (int i =0; i < 7; i++){
-				System.out.print(data[j][i]);
+		System.out.println();
+		for(int j = 0 ; j < 845; j++){
+			for (int i = 0; i < 5; i++){
+				System.out.print(london[j][i]);
 				System.out.print(", ");
 			}
 			System.out.println();
@@ -48,14 +54,35 @@ public class dataSetup {
 	
 	public static void londonSetup() {
 		
-		for (int i = 0; i < 845; i++){
-			for(int j =0; j < 5; j++){
-				data[i][3] = london[0][0];
-				
+		//extract from dataset and place in temp array
+		int track = 0;
+		for (int i = 1; i < 845; i ++) {
+			temp[track] = data[i][3];
+			System.out.print(temp[track]);
+			System.out.print(", ");	
+			track++;
+			}
+		
+		for(int j = 0; j < temp.length; j++) {
+			
+			if (first){ //on the first iteration
+				london[0][x] = temp[0];
+				first = false;
+				x++;
+				} else {
+					london[y][x] = temp[j];
+					x++;
+				}
+			if (x == 5) {
+					x = 1;
+					y++;
+					london[y][0] = temp[j];
+					}
 				}
 			}
-		}
-	}	
+		}	
+	
+		
 
 
 
