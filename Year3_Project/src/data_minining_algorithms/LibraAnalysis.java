6fb4ec93cae5ec1b47864876f59bed5fb5b5061b
Elliot <Elliotsul@aol.com>
2013-02-18 17:24:41 +0000

    Libra analysis started

--- Year3_Project/src/data_minining_algorithms/LibraAnalysis.java
+++ Year3_Project/src/data_minining_algorithms/LibraAnalysis.java
@@ -1,15 +1,17 @@
 package data_minining_algorithms;
 
-import weka.associations.*;
+import weka.core.*;
+import weka.core.converters.ConverterUtils.DataSource;
+
 
 public class LibraAnalysis {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
+		Instances data = DataSource.read("Year3_Project/Data/Request_analysis_daily.csv");
 	}
 
 }
